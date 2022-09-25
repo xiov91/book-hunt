5193dd9c7c6aa6b0fd94ee9bd80d3f8b28acefa9
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-25 18:48:23 -0400

    mutation change

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,16 +13,16 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-        email
-      }
-    }
+mutation addUser($username: String!, $password: String!, $email: String!) {
+  addUser(username: $username, password: $password, email: $email) {
+    token
+    user{ 
+      _id
+      username
+      email
   }
+  }
+}
 `;
 
 export const SAVE_BOOK = gql`
