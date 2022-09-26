14a1ce9a0b8b59087b58ae4de409909f0929b9ec
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-09-25 20:18:13 -0400

    forgot to uncomment important stuff!

--- server/server.js
+++ server/server.js
@@ -16,14 +16,14 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-/* // if we're in production, serve client/build as static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}); */
+});
 
 
 const startApolloServer = async (typeDefs, resolvers) => {
